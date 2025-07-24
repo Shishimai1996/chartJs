e12e54ca11c313c43f657f0ee1e261def4352cca
Mai Shimizu/舞 清水 <159525515+Shishimai1996@users.noreply.github.com>
2025-07-24 17:27:48 +0900

    updateee

--- chartJs/next.config.js
+++ chartJs/next.config.js
@@ -1,7 +1,7 @@
 /** @type {import('next').NextConfig} */
 const isGithubPages = process.env.GITHUB_ACTIONS === 'true'
 const nextConfig = {
-    output: 'export',
+    output: isGithubPages? 'export': undefined,
     basePath: isGithubPages? '/chartJs' : '',
     assetPrefix: isGithubPages ? '/chartJs/': ''
 }
